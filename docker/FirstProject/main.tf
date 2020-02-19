e96a8c4d0b248cd55c354699e34420ff808814ca
Oliver <ocben1@gmail.com>
2020-02-20 06:47:13 +1100

    reset head

---
+++ docker/FirstProject/main.tf
@@ -0,0 +1,35 @@
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid = "PublicReadGetObject"
+    effect = "Allow"
+    actions = [ "s3:GetObject" ]
+    principals {
+      type = "*"
+      identifiers = [ "*" ]
+    }
+    resources = [ "arn:aws:s3:::explorecalifornia.org/*" ]
+  }
+}
+
+/* We can access properties from data sources using this format:
+   ${data.<data_source_type>.<data_source_name>.<property>.
+
+   In this case, we need the JSON document, which the documentation
+   says can be accessed from the .json property. */
+
+resource "aws_s3_bucket" "website" {
+  bucket = "oliver-explorecalifornia.org"  // The name of the bucket.
+  acl    = "public-read"            /* Access control list for the bucket.
+                                       Websites need to be publicly-available
+                                       to the Internet for website hosting to
+                                       work. */
+  policy = "${data.aws_iam_policy_document.bucket_policy.json}"
+  website {
+    index_document = "index.htm"   // The root of the website.
+    error_document = "error.htm"   // The page to show when people hit invalid pages.
+  }
+}
+
+output "website_bucket_url" {
+  value = "${aws_s3_bucket.website.website_endpoint}"
+}
--- docker/FirstProject/terraform.dockerfile
+++ docker/FirstProject/terraform.dockerfile
@@ -3,6 +3,7 @@ LABEL Oliver Bennett <oliver.bennett@unimelb.edu.au>
 
 RUN wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
 RUN unzip /tmp/terraform.zip -d /
+RUN apk add ca-certificates
 
 ENTRYPOINT [ "/terraform" ]
 
